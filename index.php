bc1bcaa8af0b33d9fbc0e6cdcbf193f637d83d25
Xico <francisco100eg@gmail.com>
2023-05-11 10:26:52 -0300

    Feat: Basic Math functions

--- index.php
+++ index.php
@@ -65,13 +65,57 @@ echo "<br> Tenho {$height} de altura.";
         LogIn
     </button>
     </form>
+
+    <h1>Math fuction</h1>
+
+    <form action="index.php" method="post">
+        <label for="username">x:</label>
+        <input type="text" name="x">
+        <label for="y">y:</label>
+        <input type="text" name="y">
+        <label for="z">z:</label>
+        <input type="text" name="z">
+        <button type="submit">
+        Enviar
+    </button>
+    </form>
 </body>
 </html>
 
 <?php
-    // echo "{$_GET["username"]} <br>";
-    // echo "{$_GET["password"]} <br>";
+    echo "{$_GET["username"]} <br>";
+    echo "{$_GET["password"]} <br>";
     echo "{$_POST["username"]} <br>";
     echo "{$_POST["password"]} <br>";
-    //GET envia a informação via Url
+    // GET envia a informação via Url
+    // Post através de Body do Request Http
+
+    $x = $_POST["x"];
+    $y = $_POST["y"];
+    $z = $_POST["z"];
+    echo "O valor de x é :{$x} <br>";
+    echo "O valor de y é :{$y} <br>";
+    echo "O valor de z é :{$z} <br>";
+
+    $xabs = abs($x);
+    $yround = round($y);
+    $yfloor = floor($y);
+    $yceil = ceil($y);
+    $xpower = pow( $x, $y);
+    $xsqrt = sqrt($x);
+    $maxValue = max($x,$y,$z);
+    $minValue = min($x,$y,$z);
+    $randXY = rand($x,$y);
+
+    
+    echo "Valor absoluto de x {$xabs} <br>";
+    echo "Valor arredondado de y {$yround} <br>";
+    echo "Valor arredondado para baixo de y {$yround} <br>";
+    echo "Valor arredondado para cima de y {$yceil} <br>";
+    echo "Valor de x levantado a y {$xpower} <br>";
+    echo "Valor da raiz quadrada de x {$xsqrt} <br>";
+    echo "Valor máximo de x,y,z é : {$maxValue} <br>";
+    echo "Valor minimo de x,y,z é : {$minValue} <br>";
+    echo "Valor randomico entre x e y : {$randXY}";
+
 ?>
