55b629374a949152cee1423448f6d6e7cdd521ab
Xico <francisco100eg@gmail.com>
2023-05-11 09:32:03 -0300

    Feat: Basic Math

--- index.php
+++ index.php
@@ -1,41 +1,60 @@
-
 <?php
-    echo "Olá eu sou , <br>";
-    echo "Francisco Costa <br>";
-    //Comentário de uma linha
-    /*
+echo "Olá eu sou , <br>";
+echo "Francisco Costa <br>";
+//Comentário de uma linha
+/*
         Comentário de várias linhas
     */
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <button>
         Carrega em mim
     </button>
 </body>
+
 </html>
 
 <?php
 //Variaveis
-    $name =" Francisco Costa";
-    $email = "francisco100eg@gmail.com";
-    $age = 30;
-    $height = 1.75;
-    $total = $age * $height;
+$name = " Francisco Costa";
+$email = "francisco100eg@gmail.com";
+$age = 30;
+$height = 1.75;
+//string literal
+echo "<br> Olá eu sou {$name}.";
+echo "<br> O meu email é {$email}";
+echo "<br> Eu tenho {$age} anos.";
+echo "<br> Tenho {$height} de altura.";
 
-    echo  "$total";
-    //string literal
-    echo "<br> Olá eu sou {$name}.";
-    echo "<br> O meu email é {$email}";
-    echo "<br> Eu tenho {$age} anos.";
-    echo "<br> Tenho {$height} de altura.";
+    /*
+    Matematica simples
 
+    $total = 1 + 1
+    $total = 1 - 1
+    $total = 1 * 2
+    $total = 1 / 2
+    $total = 1 ** 2
+    $total = 1 % 2
+    */
 
+    /*
+    Incremento e Decremento
+    
+    $counter = 10;
+    $counter--
+    $counter++
+    $counter+=2
+    $counter-=2
+
+    */
 ?> 
