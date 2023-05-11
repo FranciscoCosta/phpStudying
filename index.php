bdb80cf328e4da3a7f13ffba840f72cd47307b6a
Xico <francisco100eg@gmail.com>
2023-05-11 09:58:14 -0300

    Feat: Get and Post

--- index.php
+++ index.php
@@ -6,23 +6,7 @@ echo "Francisco Costa <br>";
         Comentário de várias linhas
     */
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <button>
-        Carrega em mim
-    </button>
-</body>
-
-</html>
 
 <?php
 //Variaveis
@@ -57,4 +41,37 @@ echo "<br> Tenho {$height} de altura.";
     $counter-=2
 
     */
-?> 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <form action="index.php" method="post">
+        <label for="username">Usuário</label>
+        <input type="text" name="username">
+        <label for="password">Password</label>
+        <input type="password" name="password">
+        <button type="submit">
+        LogIn
+    </button>
+    </form>
+</body>
+</html>
+
+<?php
+    // echo "{$_GET["username"]} <br>";
+    // echo "{$_GET["password"]} <br>";
+    echo "{$_POST["username"]} <br>";
+    echo "{$_POST["password"]} <br>";
+    //GET envia a informação via Url
+?>
