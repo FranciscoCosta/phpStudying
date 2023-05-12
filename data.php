eef216828d27ba2306bc82f975bd41053f007974
Xico <francisco100eg@gmail.com>
2023-05-12 13:55:32 -0300

    Feat: Mysql Forms Registration

--- data.php
+++ data.php
@@ -2,30 +2,32 @@
 
 include("database.php");
 
-$username = "Edu";
-$password = "edu1234";
-
-// $sqlname = "SELECT * FROM users WHERE user = '$username'";
-
+// $username = "Edu";
+// $password = "edu1234";
 // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-try{
-    $result = mysqli_query($db_conn, $sqlname);
-    echo $result;
-}catch(mysqli_sql_exception $e){
-    echo "Erro ao verificar usuário <br>";
-}
+// // $sqlname = "SELECT * FROM users WHERE user = '$username'";
 
-$sql = "INSERT INTO users (users, passwords) VALUES ('$username', '$hashed_password')";
-try {
-    mysqli_query($db_conn, $sql);
-    echo "Usuário registado <br>";
-} catch (mysqli_sql_exception $e) {
-    echo "Erro ao registar usuário <br>";
-}
 
-mysqli_close($db_conn);
+// // try{
+// //     $result = mysqli_query($db_conn, $sqlname);
+// //     echo $result;
+// // }catch(mysqli_sql_exception $e){
+// //     echo "Erro ao verificar usuário <br>";
+// // }
+
+// $sql = "INSERT INTO users (users, passwords) VALUES ('$username', '$hashed_password')";
+// try {
+//     mysqli_query($db_conn, $sql);
+//     echo "Usuário registado <br>";
+// } catch (mysqli_sql_exception $e) {
+//     echo "Erro ao registar usuário <br>";
+// }
+
+// mysqli_close($db_conn);
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -38,7 +40,37 @@ mysqli_close($db_conn);
 </head>
 
 <body>
+    <form action="<?php ($_SERVER["PHP_SELF"]) ?>" method="post">
+    <h1>Registo de usuários</h1>
+    <label for="username">username</label>
+    <input type="text" name="username" id="username">
+    <label for="password">password</label>
+    <input type="password" name="password" id="password">
+    <button type="submit" name="submit" value="register">Cadastrar</button>
+    </form>
 
 </body>
 
 </html>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+    if(empty($username) || empty($password)){
+        echo "Preencha todos os campos";
+}
+    $sql = "INSERT INTO users (users, passwords) VALUES ('$username', '$hashed_password')";
+    try {
+        mysqli_query($db_conn, $sql);
+        echo "Usuário registado <br>";
+    } catch (mysqli_sql_exception $e) {
+        echo "Erro ao registar usuário <br>";
+    }
+    mysqli_close($db_conn);
+}
+
+    ?>
