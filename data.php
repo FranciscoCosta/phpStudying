742c427e4a7c1dc9e027b89f8eefb94c55ecd6c7
Xico <francisco100eg@gmail.com>
2023-05-12 11:47:27 -0300

    Feat: Mysql database data sql queries

--- data.php
+++ data.php
@@ -1,12 +1,36 @@
+<?php
+
+include("database.php");
+
+$username = "Edu";
+$password = "edu1234";
+
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+
+$sql = "INSERT INTO users (users, passwords) VALUES ('$username', '$hashed_password')";
+try {
+    mysqli_query($db_conn, $sql);
+    echo "Usuário registado <br>";
+} catch (mysqli_sql_exception $e) {
+    echo "Erro ao registar usuário <br>";
+}
+
+mysqli_close($db_conn);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
